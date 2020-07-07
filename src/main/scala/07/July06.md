4fa6dfaceb953b5a9f1bba97abada38da5d1b9de
nozomi.yonezawa <nozomi.yonezawa@nextbeat.net>
2020-07-07 10:24:38 +0900

    add file July07

--- src/main/scala/07/July06.md
+++ src/main/scala/07/July06.md
@@ -1,5 +1,16 @@
 # sbt 
-- scala用のビルドツール
+scala用のビルドツール
+
+
+
+# 関数とメゾットの違い
+
+メゾット：defで始まる構文
+関数　　：第一級オブジェクト。オブジェクトとして扱えるので、「変数に代入する」「メゾットや関数の引数として渡す」「メゾットや関数の戻り値として返す」もOK
+
+参考：
+https://sites.google.com/site/scalajp/home/documentation/scala-by-example/chapter5
+http://www.ne.jp/asahi/hishidama/home/tech/scala/function.html
 
 
 # Option
@@ -7,6 +18,54 @@
 - 型の一種
 - return Some[T] or None
 - 使用例：「存在しない」「条件に合うものを発見できない」を探すとき
+
+
+
+## メゾット一覧
+
+-　get
+  -　返り値：値
+  -  注意　：Opitionの値がNoneの場合は例外が発生するため使用しない
+
+-　getOrElse(引数)
+  -　返り値：Optionの値 or 引数の値
+
+-　isEmpty 
+  -　返り値：true or false
+  -　メモ　：Option[Option[Int]]にも使える。（参照：sample)
+
+- isDefined
+  -  返り値：true or false
+  -　メモ　：Option[Option[Int]]にも使える。（参照：sample)
+
+
+- map()
+  -　返り値：Some or None
+　-　引数　：関数      ex) i => i * 2
+　-　仮引数には、mapの返り値（つまりOption型の値 or None)が代入されている
+  -  Option[Option[Int]]が渡された場合、Option[Option[int]]で返す
+
+
+- flatten
+  -  返り値：Some or None
+  -  Option[Option[Int]]に対して使う
+
+- flatMap()
+  -  返り値：Some or None
+  -  引数　：関数　　ex) A => Option[B]
+  -  Option[Option[Int]]が渡された場合、Option[Int]で返す
+
+
+## メゾットのいろんな書き方
+
+val intval: Option[Int] = Some(5)
+intval.map(i => i * 2)
+intval.map(i => {i * 2})
+intval.map(_ * 2)
+intVal map(i => i * 2)
+
+
+
 
 
 ## get method
@@ -17,25 +76,35 @@
 - 値が存在しない可能性が存在しない可能性がある時は、getOrElseメゾットを使う
 
 
+
 ## getOrElse method
+
 - Optionの値が存在している場合(Someの場合)はその値を返却し、存在していない場合(Noneの場合)は、引数として受け取った値を返却
 
 
+
 ## isEmpty method
+
 - Optionの有無をBooleanで返す
 - Noneの場合はtrue
+- Some("")のときは、false
 
 
-## isDifined method
+
+## isDefined method
+
 - Optionの有無をBooleanで返す 
 - Some[T]の場合にtrue
+- Some("")のときは、true
+
 
 
 ## map method
-- 関数（式）を引数に取り、SomeかNoneを返す
-- 値が存在する場合、Some()の中に関数結果を入れて返す
-- 引数は、Optionの内包する型と同一である必要がある
-- 返り値は任意の型
+
+-　引数：関数（式）
+-　返り値：SomeかNone
+-  引数は、Optionの内包する型と同一である必要がある
+-  返り値は任意の型
 
 
 ```
@@ -52,6 +121,8 @@ val res1: Option[Int] = Some(10)
 
 
 ## flatten method
+
+- 返り値：Some / None
 - Option[Option[A]] のように Option型が入れ子になっている場合に、 平滑化するためのメソッド
 - Option が入れ子になるケースは map が受け取る引数が A => Option[B] のような場合に起こり得ます
 
@@ -72,17 +143,25 @@ scala> intVal.flatten
 val res8: Option[Int] = None
 ```
 
+
 ## flatMap method
+
 -  map + flatten 
--  引数は、A => Option[B] の型
--  返り値の型は、Option[B] 
+-  引数：A => Option[B]
+-  返り値：Option[B] 
+
 
 
 ### mapとflatMapの違い
+
 引数が、A => Option[B] のとき
 
 mapの返り値：Option[Option[B]] 
 flatMapの返り値:Option[B] 
+
+
+##
+in many situations, you can ignore the difference between functions and methods and just think of them as the same thing, but occasionally you may run into a situation in which the difference matters. 
 
 
 
--- src/main/scala/07/July06.scala
+++ src/main/scala/07/July06.scala
@@ -1,15 +1,138 @@
-object July 06 {
+object July06 {
 
-  def main(args; Array[String]); Unit + {
-    println(test)
+  def main(args: Array[String]): Unit = {
+    println("//Test")
+    println(sample(Some(Some(10))))
+    println(sample2(Some(10)))
+    println(sample3(Some(2)))
+    println(sample4(Some(2)))
+
+
+    println("//Option Question1")
+    println(optionQuestion1(Some(1)))
+    println(optionQuestion1(None))
+    
+    println("//Option Question2")
+    println(optionQuestion2(Some(0)))
+    println(optionQuestion2(None))
+
+    println("//Option Question3")
+    println(optionQuestion3())
+
+
+    println("//Option Question4")
+    println(optionQuestion4(Some(Some(2))))
+    println(optionQuestion4(None))
+    println(optionQuestion4(Some(None)))
+
+    println("//Option Question4: flatMap + map")    
+    println(optionQuestion4_2(Some(Some(2))))
+    println(optionQuestion4_2(None))
+    println(optionQuestion4_2(Some(None))) 
+
+    
+    println("//Option Question4: for")
+    //println(optionQuestion4_3(Some(Some(2))))
+    //println(optionQuestion4_3(None))
+    //println(optionQuestion4_3(Some(None))) 
+
   }
 
-  dest test() + {
-    val num: Option[Int] = Some(1)
-    num.getOrElse(2) // -> Int = 1
 
-    val num2: Option[Int] = None
-    num2.getOrElse(2) // -> None
+  // 練習
+  def sample(item: Option[Option[Any]]): Boolean = {
+    item.isEmpty
+    item.isDefined
+  } 
+ 
+  def sample2(num: Option[Int]) = {
 
+    num.map(i => i * 2)
+
+  }
   
+  def sample3(num: Option[Int]) = {
+  
+    val option1 = Some(1)
+    val option2 = Some(2)
+    val option3 = Some(3)
+  
+    for {
+
+       i1 <- option1
+       i2 <- option2
+       i3 <- option3
+  
+     } yield {
+    
+      i1 * i2 * i3
+
+    }
+
+  }
+
+  def sample4(num: Option[Int]) = {
+    
+    val option1 = Some(1)
+    val option2 = Some(2)
+    val option3 = Some(3)
+
+    option1 flatMap { i1 =>
+      
+      option2 flatMap { i2 =>
+      
+        option3 map { i3 =>
+      
+          i1 * i2 * i3
+    
+        }
+      }
+    }
+
+  }
+
+
+
+
+
+  // 問題1
+  // Option[Int]型である、numが引数として渡されるメソッドoptionQuestion1があります。Someの場合はその値を、Noneの場合は０を返却するメソッドを、getOrElseメソッドを使って作成してください。
+  def optionQuestion1(num: Option[Int]): Int = {
+    num.getOrElse(0)
+  }
+
+
+  // 問題２
+  // Option[Int]型である、numが引数として渡されるoptionQuestion2メソッドがあります。Someの場合はその値に3を足したSome[Int]を、Noneの場合はそのままNoneを返却するメソッドを、mapメソッドを使って作成してください。
+  def optionQuestion2(num: Option[Int]): Option[Int] = {
+    num.map(_ + 3)
+  }
+
+
+ // 問題３
+ // val strOpt: Option[String] = Some("") を定義し、それに対してisEmpty, isDefined を実行した場合にどのような挙動をするか確かめてください。
+  def optionQuestion3(): Unit = {
+    val strOpt: Option[String] = Some("")
+
+    println(s"isEmpty = ${strOpt.isEmpty}")
+    println(s"isDefined = ${strOpt.isDefined}")  
+  }
+  
+
+  // 問題４
+  // Option[Option[Int]]型である、numが引数として渡されるoptionQuestion4メソッドがあります。Someの場合はその値を2倍したSome[Int]を、Noneの場合はそのままNoneを返却するメソッドを作成してください。
+  def optionQuestion4(num: Option[Option[Int]]): Option[Int]  = {
+    num.flatMap(i => if (i.isDefined) Some(i.get * 2) else None)
+  }
+
+  def optionQuestion4_2(num: Option[Option[Int]]): Option[Int] = {
+    num flatMap(i => i map(_ * 2))
+  }
+  
+  //def optionQuestion4_3(num: Option[Option[Int]]): Option[Int] = {
+    
+  //}
+
+
+
 }
