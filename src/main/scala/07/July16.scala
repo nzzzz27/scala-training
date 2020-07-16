5d19ad59aadf96f71d702e612438cac61e29568f
yonezawa <nozomi.yonezawa@nextbeat.net>
2020-07-16 19:17:22 +0900

    July16 19:00 [Either, Try]

--- src/main/scala/07/July16.scala
+++ src/main/scala/07/July16.scala
@@ -13,13 +13,21 @@ object July16 {
     println(s"直方体: $rectangularVolume")
     println(s"四角錐: $fourSidedPyramidVolume")
     
-    println("//--- Either, Try --")
+    println("//--- Either, Try Test --")
     println(s"Either定義１: $leftEither, $rightEither")
-    println(s"値取得:  ${leftEither.left.get}")
+    println(s"leftEither.left.get:  ${leftEither.left.get}")
+    println(s"rightEither.left.getOrElse(0):  ${rightEither.left.getOrElse(0)}")
+    println(s"rightEither.left.map(i => i):  ${rightEither.left.map(i => i)}")
+    println(s"leftEither.right.getOrElse(0):  ${leftEither.right.getOrElse(0)}")
+    println(s"leftEither.right.map(i => i):  ${leftEither.right.map(i => i)}")
+    println(tom.name)
+
+    println("//--- Either, Try Question --")
+    println(s"eitherTryQuestion1_A: $eitherTryQuestion1_A")
+    println(s"eitherTryQuestion1_B: $eitherTryQuestion1_B")
+
   }
   
-  
-  //--- case class tuple ---
   val caseClasstest = Company("nextbeat", "03-5423-6131", None)
 
   //---- case class, tuple  Questions ----
@@ -70,9 +78,44 @@ object July16 {
   val rectangularVolume     : Int = Rectangular(8, 6, 6).calculate
   val fourSidedPyramidVolume: Int = FourSidedPyramid(8, 6, 6).calculate
   
-  // --- Either, Try ---
+  // --- Either test ---
 
   val leftEither : Either[String, Int] = Left("string") //Left(string)
   
   val rightEither: Either[String, Int] = Right(100) //Right(100)
+  case class Person(name: String, age: Int)
+  val tom = Person("Tom", 20)
+
+
+  // --- Either, Try Questions ---
+  //Q1
+  /*
+    エディタ、IDE、sbtなどを使わずに、以下の構文でのEitherの型を答えてください。ただし、Eitherのソースを読むのは可とします。
+    
+    Either[A, B]の、AとBをそれぞれ答えられたらOKです。
+       
+    定義
+    case class User(id: UserId, name: String)
+    case class ExceptionMessage(message: String)
+
+    object LoginApplication {
+      def login(id: UserId): Either[String, User] = {
+        // implement
+      }
+    }
+    
+    回答対象コード
+    // val loginRes = Either[String, User]型の値
+    // 
+    val loginRes = login(UserId(1))
+    loginRes.left.map(s => ExceptionMessage(s)).right.map(u => u.id)
+  */
+  val eitherTryQuestion1_A: String = "A: "
+  val eitherTryQuestion1_B: String = "B: "
+  
+  //loginRes.left         : NoSuchElementException
+  //loginRes.left.map(s ~): Right()
+  //ExceptionMessage(s).right.map(u ~):
+
+
 }
